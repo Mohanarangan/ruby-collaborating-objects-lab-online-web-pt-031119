33c076fa24bd52a5baf907ec8304a341e38e7b24
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-23 09:44:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,5 @@
 class Artist 
-  attr_accessor :name, :songs
-  
-  @@all = []
+  attr_accessor :name, :songs 
   
   def initialize(name)
     @name = name
@@ -9,31 +7,7 @@ class Artist
   end
   
   def add_song(song)
-    songs << song
+    @songs << songs
   end
-  
-  def self.all
-    @@all
-  end
-  
-  def save
-    @@all << self 
-    
-  end
-  
-  def self.find_or_create_by_name(name)
-    if @@all.find { |artist| artist.name==name }
-       @@all.find { |artist| artist.name==name }
-    else
-      new_artist = self.new(name)
-      new_artist.save 
-      new_artist
-    end
-  end
-  
-  def print_songs
-    songs.each {|song| puts song.name}
-  end
-  
   
 end
